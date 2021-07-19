d1dff9bcd0378d2b0fee7a8c294335c1dd849d57
Igor Bohn <igorfbohn@gmail.com>
2021-07-19 17:44:58 -0300

    Testes em andamento

---
+++ scripts/perguntas-frequentes.js
@@ -0,0 +1,29 @@
+/*
+
+var btn = document.querySelector('.perguntas-frequentes__btn');
+
+var resposta = document.querySelector('.perguntas-frequentes__resposta');
+
+btn.addEventListener('click', function() {
+
+    if(resposta.style.display === 'none') {
+        resposta.style.display = 'block';
+    }
+    else {
+        resposta.style.display = 'none';
+    }
+
+});
+
+*/
+
+
+var btn = document.querySelector('.perguntas-frequentes__btn');
+
+var resposta = document.querySelector('.perguntas-frequentes__resposta');
+
+btn.addEventListener('click', function() {
+
+    
+
+})
