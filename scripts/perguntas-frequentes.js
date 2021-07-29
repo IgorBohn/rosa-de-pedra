32d4a4e5f9d958c115c3eb7bee07d212e595cffe
Igor Bohn <igorfbohn@gmail.com>
2021-07-29 16:49:54 -0300

    Ajustado seção -Perguntas Frequentes-

--- scripts/perguntas-frequentes.js
+++ scripts/perguntas-frequentes.js
@@ -1,10 +1,34 @@
-const pergunta = document.querySelectorAll(".pergunta");
+const pergunta = document.querySelectorAll(".perguntas-frequentes__pergunta");
+const seta = document.querySelectorAll(".perguntas-frequentes__seta");
 
+/*
 pergunta.forEach((pergunta) => {
   pergunta.onclick = function () {
-    this.classList.toggle("is-open");
+    let content = this.nextElementSibling;
+    content.classList.toggle("is-open");
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+
+    seta.forEach((seta) => {
+      seta.onclick = function () {
+        seta.classList.toggle("seta-rotate");
+    
+      };
+    });
+  };
+});
+*/
+
+seta.forEach((seta) => {
+  seta.onclick = function () {
+    seta.classList.toggle("seta-rotate");
 
     let content = this.nextElementSibling;
+    content.classList.toggle("is-open");
 
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
